1945f5a35d127918514ff9c987fe444fce2733de
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-08-31 11:12:13 +0300

    Updates

---
+++ precache-manifest.f3ac431d416f6bd706f193c16224ce3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5130893ae46383fb168ed4b2b49ddd4f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ebb41580dd51017fc25",
+    "url": "./static/js/2.dd291650.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.dd291650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e9da6b6970bb19cda9",
+    "url": "./static/js/main.c8c7fc86.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.f3ac431d416f6bd706f193c16224ce3f.js"
 );
 
 self.addEventListener('message', (event) => {
